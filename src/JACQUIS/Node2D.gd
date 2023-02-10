d65dacf7be88a9114283a64eae875c22da66a3f0
Leandro672 <leandro.gomes@sou.inteli.edu.br>
2023-02-10 09:59:08 -0300

    Adição MVP

--- src/JACQUIS/Node2D.gd
+++ src/JACQUIS/Node2D.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-var x = 2.5
-var vel = 1.5
+# Variaveis
+var car = 2.5
+var truck = 1.5
 
 # executa essa função ao carregar o jogo
 func _ready():
@@ -14,30 +14,30 @@ func _ready():
 func _process(delta):
 	
 	#Faz o carro verde ir para a esquerda e reaparecer 
-	$carro_ver.position.x -= 2*x
+	$carro_ver.position.x -= 2*car
 	if ($carro_ver.position.x) < -200:
 			($carro_ver).position.x = 1700
 	
 	#Faz o carro vermelho ir para a esquerda e reaparecer
-	$carro_red.position.x -= 2*x
+	$carro_red.position.x -= 2*car
 	if ($carro_red.position.x) < -200:
 			$carro_red.position.x = 1700
 	
 	#Movimento do caminhão para a direita
 	if Input.is_action_pressed("ui_right"):
-			$Caminhao.position.x += 2*vel
+			$Caminhao.position.x += 2*truck
 		
 	#Movimento do caminhão para a esquerda
 	if Input.is_action_pressed("ui_left"):
-			$Caminhao.position.x -= 2*vel
+			$Caminhao.position.x -= 2*truck
 	
 	#Movimento do caminhão para cima 
 	if Input.is_action_pressed("ui_up"):
-			$Caminhao.position.y -= 2*vel
+			$Caminhao.position.y -= 2*truck
 		
 	#movimento do caminhão para baixo
 	if Input.is_action_pressed("ui_down"):
-			$Caminhao.position.y += 2*vel
+			$Caminhao.position.y += 2*truck
 			
 			
 			
