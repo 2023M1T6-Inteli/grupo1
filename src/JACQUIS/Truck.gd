4516d0f8c88a10fecb50db8c6c73c0a78a894e87
Felipe Liberman Fuchs <felipe@almosthackers.net>
2023-02-13 15:10:38 -0300

    restrict truck movement to Y axis

--- src/JACQUIS/Truck.gd
+++ src/JACQUIS/Truck.gd
@@ -10,18 +10,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    #Movimento do caminhão para a direita
-	if Input.is_action_pressed("ui_right"):
-			self.position.x += truckXDeltaSpace
-
-	#Movimento do caminhão para a esquerda
-	if Input.is_action_pressed("ui_left"):
-			self.position.x -= truckXDeltaSpace
-
 	#Movimento do caminhão para cima
 	if Input.is_action_pressed("ui_up"):
-			self.position.y -= truckYDeltaSpace
+			position.y -= truckYDeltaSpace
 
 	#movimento do caminhão para baixo
 	if Input.is_action_pressed("ui_down"):
-			self.position.y += truckYDeltaSpace
+			position.y += truckYDeltaSpace
