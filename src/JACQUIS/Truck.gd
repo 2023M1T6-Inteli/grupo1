550446b0d0df4b92d7a907257288dc5e1bbb8ccd
Felipe Liberman Fuchs <felipe@almosthackers.net>
2023-02-13 15:42:50 -0300

    add Y-axis collision detection with background

--- src/JACQUIS/Truck.gd
+++ src/JACQUIS/Truck.gd
@@ -1,19 +1,27 @@
 extends KinematicBody2D
 
-var truckXDeltaSpace = 3
-var truckYDeltaSpace = 3
+const Y_DELTA = 250
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#Movimento do caminhão para cima
-	if Input.is_action_pressed("ui_up"):
-			position.y -= truckYDeltaSpace
+	pass
 
-	#movimento do caminhão para baixo
-	if Input.is_action_pressed("ui_down"):
-			position.y += truckYDeltaSpace
+# Use _physics_process(delta) when using Kinematic2D (according to docs)
+func _physics_process(delta):
+	var dislocation = Vector2(0, 0)
+
+	# Movimento do caminhão para cima
+	if Input.is_action_pressed("ui_up"):
+		dislocation.y = -1 * Y_DELTA
+
+	# Movimento do caminhão para baixo
+	elif Input.is_action_pressed("ui_down"):
+		dislocation.y = Y_DELTA
+
+	# Faz o movimento com colisão em 2D
+	move_and_slide(dislocation)
