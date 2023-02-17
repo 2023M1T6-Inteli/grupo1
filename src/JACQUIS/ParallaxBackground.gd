e175f98ca1bddfc2ecb0d257d46ba45a99de23a7
Felipe Liberman Fuchs <felipe@almosthackers.net>
2023-02-17 10:17:53 -0300

    setup scene dynamic speed

--- src/JACQUIS/ParallaxBackground.gd
+++ src/JACQUIS/ParallaxBackground.gd
@@ -1,5 +1,11 @@
 extends ParallaxBackground
-export (float) var scrolling_speed = 500.0
+export (float) var scrolling_speed = 250.0
 
 func _process(delta):
+	# Gradually increases the speed of the game up to 500.0
+	if scrolling_speed <= 500:
+		scrolling_speed += delta
+		print(scrolling_speed)
+
 	scroll_offset.x -= scrolling_speed * delta
+	
