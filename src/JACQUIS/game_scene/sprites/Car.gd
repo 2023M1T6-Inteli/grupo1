4873ef3af3d0a9f2590433030c3d6f4157cf177f
Felipe Liberman Fuchs <felipe@almosthackers.net>
2023-02-23 15:18:09 -0300

    create Hole and Puddle

--- src/JACQUIS/game_scene/sprites/Car.gd
+++ src/JACQUIS/game_scene/sprites/Car.gd
@@ -1,16 +1,15 @@
 extends KinematicBody2D
 
-var rng = RandomNumberGenerator.new()
+
 var moving = true
 const ROW_XVALS = { 0: 302, 1: 411, 3: 527}
-var sprite_speed = 225.0
+# We add 12% to the sprite_speed so it doesn't seem like it's stuck to the background
+var sprite_speed = 225.0 * 1.22
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rng.randomize()
 	if sprite_speed <= 750 and OS.get_ticks_msec() > 10000:
 		sprite_speed += int(floor((OS.get_ticks_msec() - 10000) / 5000.0))
-		sprite_speed *= rng.randf_range(1.15, 1.30)
 
 func _physics_process(delta):
 	if (moving):
