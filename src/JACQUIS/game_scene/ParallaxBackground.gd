87a6c1afcfc2c0c75a8017d3fe91e5e329bd9e25
Felipe Liberman Fuchs <felipe@almosthackers.net>
2023-02-23 14:20:04 -0300

    make speed proportional to msec ticks

--- src/JACQUIS/game_scene/ParallaxBackground.gd
+++ src/JACQUIS/game_scene/ParallaxBackground.gd
@@ -1,5 +1,5 @@
 extends ParallaxBackground
-export (float) var scrolling_speed = 250.0
+export (float) var scrolling_speed = 225.0
 
 # Scrolls the background by an offset on X
 func scrollBackground(offset):
@@ -9,7 +9,7 @@ func _process(delta):
 	_increaseScrollingSpeed(delta)
 	scrollBackground(scrolling_speed * delta)
 
-# Gradually increases the speed of the game up to 500.0
+# Gradually increases the speed of the game up to 500.0 after 20s of game
 func _increaseScrollingSpeed(delta):
-	if scrolling_speed <= 500:
-		scrolling_speed += delta
+	if scrolling_speed <= 500 and OS.get_ticks_msec() > 20000:
+		scrolling_speed += int(floor((OS.get_ticks_msec() - 20000) / 1000.0))
