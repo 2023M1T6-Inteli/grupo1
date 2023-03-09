540fc54f3245e242ede1bcd957e7dc2a94c730a4
Biabborges <bianca.lins@sou.inteli.edu.br>
2023-03-09 08:58:09 -0300

    Incluindo o "X" na checklist

--- src/JACQUIS/assets/ChecklistScene.gd
+++ src/JACQUIS/assets/ChecklistScene.gd
@@ -1,28 +1,41 @@
 extends Node2D
 
 
-
 # Called when the node enters the scene tree for the first time.
-func _process(delta):
-	pass
+func _ready():
+	var xses = [$X1, $X2, $X3, $X4, $X5, $X6]
+	# Esconder todos os checkboxes de "errado"
+	for x in xses:
+		x.visible = false
 
 
 func _on_Button_pressed():
+	var checkboxes = [$CheckBox, $CheckBox2, $CheckBox3, $CheckBox4, $CheckBox5, $CheckBox6]
+	var xses = [$X1, $X2, $X3, $X4, $X5, $X6]
 	var something_wrong = false
-	var correct_checkboxes = [$CheckBox, $CheckBox3, $CheckBox5]
-	var wrong_checkboxes = [$CheckBox2, $CheckBox4, $CheckBox6]
+	var correct_items = [1, 3, 5]
+	var wrong_items = [2, 4, 6]
 	
-	for checkbox in wrong_checkboxes:
-		print(checkbox.text)
-		if checkbox.is_pressed():
+	# 1: Esconder todos os checkboxes de "errado"
+	for x in xses:
+		x.visible = false
+	
+	# 2: Ver se tem algum item errado checkado
+	for i in wrong_items:
+		i -= 1 # Corrige o valor de i para chegar ao índice da Array
+		if checkboxes[i].is_pressed():
 			# Mostrar vermelinho na checkbox
 			something_wrong = true
+			xses[i].visible = true # Mostrar o X vermelho
 			
-	for checkbox in correct_checkboxes:
-		if !checkbox.is_pressed():
+			
+	# 3: Ver se tem algum item certo não checkado
+	for i in correct_items:
+		i -= 1 # Corrige o valor de i para chegar ao índice da Array
+		if !checkboxes[i].is_pressed():
 			# Mostrar vermelinho na checkbox
 			something_wrong = true
-			
+			xses[i].visible = true # Mostrar o X vermelho
 
 		
 	if !something_wrong:
