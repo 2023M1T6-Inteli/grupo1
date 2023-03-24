b5097fdd57ba0ae7f13006c4197e91809a9e4c99
Leandro672 <leandro.gomes@sou.inteli.edu.br>
2023-03-24 09:44:57 -0300

    Atualização JACQUIS
    Conserto da tela de gameover e texto dos minigames

--- src/JACQUIS/Global.gd
+++ src/JACQUIS/Global.gd
@@ -2,3 +2,4 @@ extends Node
 
 var nome = ""
 var score = 0 
+var vida = 0
--- src/JACQUIS/scenes/intro_scene/intro_scene.gd
+++ src/JACQUIS/scenes/intro_scene/intro_scene.gd
@@ -1,4 +1,5 @@
 extends Node2D
 
 func _ready():
+	Global.score = 0
 	$Label2.text = "Olá " + Global.nome + "! Sou o Jacquis. Vou te instruir durante essa jornada, desenvolvida por alunos do Inteli."
